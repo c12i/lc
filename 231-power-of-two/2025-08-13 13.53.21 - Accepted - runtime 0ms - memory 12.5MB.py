1fd09ff1d0bdab1fbc673d45cc261e1d5897447c
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-08-13 13:53:21 +0300

    add 231-power-of-two

---
+++ 231-power-of-two/2025-08-13 13.53.21 - Accepted - runtime 0ms - memory 12.5MB.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # if n == 0:
+        #     return False
+
+        # while n % 2 == 0:
+        #     n = n // 2
+
+        # if n == 1:
+        #     return True
+        # else:
+        #     return False
+        
+        return n > 0 and (n & (n - 1)) == 0
