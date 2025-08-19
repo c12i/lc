ff36d91d1633887cfc384d67c6a70f2b562f79c1
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-08-20 00:00:55 +0300

    add 148-sort-list

---
+++ 148-sort-list/2025-08-20 00.00.55 - Accepted - runtime 434ms - memory 50.6MB.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        return self.merge_sort(head)
+
+    def merge_sort(self, head):
+        if not head or not head.next:
+            return head
+
+        # Split list into halves
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None  # Split the list
+
+        left = self.merge_sort(head)
+        right = self.merge_sort(mid)
+
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        tail = dummy
+    
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+    
+        tail.next = l1 or l2
+        return dummy.next
