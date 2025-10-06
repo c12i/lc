397a4709c019eabe46cd29cee041c17104948524
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-10-06 17:03:32 +0300

    add 82-remove-duplicates-from-sorted-list-ii

---
+++ 82-remove-duplicates-from-sorted-list-ii/2025-10-06 17.03.32 - Accepted - runtime 3ms - memory 12.4MB.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head):
+        dummy = ListNode(0, head)
+
+        curr = dummy.next
+        prev = dummy
+
+        while curr and curr.next:
+            if curr.next and curr.val == curr.next.val:
+                dup = curr.val
+                while curr and curr.val == dup:
+                    curr = curr.next
+                prev.next = curr
+            else:
+                prev = curr
+                curr = curr.next
+
+        return dummy.next
+        
