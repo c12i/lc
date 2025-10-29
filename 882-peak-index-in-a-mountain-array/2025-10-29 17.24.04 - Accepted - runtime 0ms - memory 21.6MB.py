815384efaab0adfdb71e62f3a29238283157b3d4
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-10-29 17:24:04 +0300

    add 882-peak-index-in-a-mountain-array

---
+++ 882-peak-index-in-a-mountain-array/2025-10-29 17.24.04 - Accepted - runtime 0ms - memory 21.6MB.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        low = 0
+        high = len(arr) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] < arr[mid + 1]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
