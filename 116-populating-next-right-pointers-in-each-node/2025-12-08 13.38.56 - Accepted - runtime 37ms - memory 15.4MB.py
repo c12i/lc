dad4119ad0b45684ea60d624380215bf825d496e
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-12-08 13:38:56 +0300

    add 116-populating-next-right-pointers-in-each-node

---
+++ 116-populating-next-right-pointers-in-each-node/2025-12-08 13.38.56 - Accepted - runtime 37ms - memory 15.4MB.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root:
+            return
+
+        q = deque([root])
+    
+        while q:
+            n = len(q)
+            
+            for i in range(n - 1):
+                q[i].next = q[i + 1]
+
+            for i in range(n):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return root
